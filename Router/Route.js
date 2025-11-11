fe5834c764e0ea85d1fa6ddcdaf4ede8e71c99c3
Elodie <elodie.devweb@gmail.com>
2025-11-11 23:33:18 +0100

    Routing system added

---
+++ Router/Route.js
@@ -0,0 +1,13 @@
+export default class Route {
+    constructor(url, title, pathHtml, authorize, pathJS = "") {
+      this.url = url;
+      this.title = title;
+      this.pathHtml = pathHtml;
+      this.pathJS = pathJS;
+      this.authorize = authorize;
+    }
+}
+
+/* Role definition
+[] -> open to everybody
+*/
---
+++ Router/allRoutes.js
@@ -0,0 +1,15 @@
+import Route from "./Route.js";
+
+//Routes
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html", []),
+    /* to update
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "js/auth/signin.js"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html", [""] ),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", [""]),
+    */
+];
+
+//title : Route.title - websitename
+export const websiteName = "EcoRide";
