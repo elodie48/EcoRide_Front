d2a106b9fe9fc652a8f45f1a4608f01845beae83
Elodie <elodie.devweb@gmail.com>
2025-11-13 23:46:41 +0100

    Footer added

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,12 +3,13 @@ import Route from "./Route.js";
 //Routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", []),
-    /* to update
+    /*
     new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "js/auth/signin.js"),
     new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html", [""] ),
-    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", [""]),
-    */
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", [""]), */
+    new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/legalNotices", "Mentions légales", "/pages/legalNotices.html", []),
 ];
 
 //title : Route.title - websitename
