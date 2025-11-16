45a75f59dd111b8e9ba9875e30f3e52a6f167cb0
Elodie <elodie.devweb@gmail.com>
2025-11-17 00:12:07 +0100

    editPassword page created

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,8 +6,7 @@ export const allRoutes = [
     new Route("/signin", "Connexion", "/pages/auth/signin.html", []),
     new Route("/signup", "Inscription", "/pages/auth/signup.html", []),
     new Route("/account", "Mon compte", "/pages/auth/account.html", [] ),
-    /*
-    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", [""]), */
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", []),
     new Route("/contact", "Contact", "/pages/contact.html", []),
     new Route("/legalNotices", "Mentions légales", "/pages/legalNotices.html", []),
 ];
