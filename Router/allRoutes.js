acb2506604c310c45dd8a0a0500acefe3908350e
Elodie <elodie.devweb@gmail.com>
2025-11-16 23:48:31 +0100

    Account page created

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,8 +5,8 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", []),
     new Route("/signin", "Connexion", "/pages/auth/signin.html", []),
     new Route("/signup", "Inscription", "/pages/auth/signup.html", []),
-    
-    /*new Route("/account", "Mon compte", "/pages/auth/account.html", [""] ),
+    new Route("/account", "Mon compte", "/pages/auth/account.html", [] ),
+    /*
     new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", [""]), */
     new Route("/contact", "Contact", "/pages/contact.html"),
     new Route("/legalNotices", "Mentions légales", "/pages/legalNotices.html", []),
