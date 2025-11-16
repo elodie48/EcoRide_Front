289f72b93b0ebd25c85201c6772ea062260eee4e
Elodie <elodie.devweb@gmail.com>
2025-11-17 00:06:12 +0100

    Contact page modified

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,7 +8,7 @@ export const allRoutes = [
     new Route("/account", "Mon compte", "/pages/auth/account.html", [] ),
     /*
     new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", [""]), */
-    new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/contact", "Contact", "/pages/contact.html", []),
     new Route("/legalNotices", "Mentions légales", "/pages/legalNotices.html", []),
 ];
 
