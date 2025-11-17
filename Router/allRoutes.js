49473ceb2faca54d7a7dcb4381c0f5237fc1ace3
Elodie <elodie.devweb@gmail.com>
2025-11-17 01:01:07 +0100

    JS signin, signup, script pages added.

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,8 +3,8 @@ import Route from "./Route.js";
 //Routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", []),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html", []),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html", []),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", [], "js/auth/signin.js"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", [], "js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html", [] ),
     new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", []),
     new Route("/contact", "Contact", "/pages/contact.html", []),
